645580437d9c207f65ac54ab010e71701f3fd8ff
Dominic Orchard <dom.orchard@gmail.com>
2025-07-03 17:41:31 +0100

    smoke test cleanup

--- example/tests/smoke_test.py
+++ example/tests/smoke_test.py
@@ -1,20 +1,11 @@
-import energy_balance
-
 # Some common package checks
-
 def test_initialization():
-    from src import __init__ as package_init
+    from energy_balance import __init__ as package_init
     assert package_init is not None, "Package initialization failed"
 
-def test_version():
-    from src import __version__
-    assert __version__ is not None, "Version should not be None"
-    assert isinstance(__version__, str), "Version should be a string"
-    assert __version__ != "", "Version should not be an empty string"
-
 # Main smoke test for energy_balance package
-
 def test_config_loading():
+    import energy_balance
     # Check that init does not fail
     energy_balance.init()
     # and that it initializes a global variable
