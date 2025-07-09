a20d8efdcd837900baf26309c153cf9fbecb2e12
Dominic Orchard <dom.orchard@gmail.com>
2025-07-09 13:40:11 +0100

    more black

--- example/src/observation.py
+++ example/src/observation.py
@@ -1,18 +1,19 @@
-from energy_balance import temperature_at_energy_balance
 from numpy import ndarray
 
-def mean_squared_error(model : ndarray, observed : ndarray) -> float:
-  n = len(observed)
-  if len(model) != len(observed):
-    raise(ValueError("Model length and observation length should match"))
-  else:
-    if n == 0:
-      return 0
+
+def mean_squared_error(model: ndarray, observed: ndarray) -> float:
+    n = len(observed)
+    if len(model) != len(observed):
+        raise (ValueError("Model length and observation length should match"))
     else:
-      sum = 0
-      for m, o in zip(model, observed):
-        sum += (m - o)**2
-      return (1/n) * sum
+        if n == 0:
+            return 0
+        else:
+            sum = 0
+            for m, o in zip(model, observed):
+                sum += (m - o) ** 2
+            return (1 / n) * sum
+
 
 # todo: get some observational data relating albedo and temperature over time
 observed = [(0.3, 15.5), (0.4, 20.0)]
